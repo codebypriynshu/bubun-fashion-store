018267989da448a323da58f2e6befd2630c7ad97
Priyanshu Ghosh <codewithxido@Priyanshus-MacBook-Air.local>
2026-06-19 22:56:26 +0530

    Intital commit -Bubun Store

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+
+        <div>
+            <img src={assets.logo} className='mb-5 w-32' alt="" />
+            <p className='w-full md:w-2/3 text-gray-600'>
+            Bubun Store is your trusted destination for stylish fashion, premium quality products, and affordable prices. We are committed to bringing you the latest trends with excellent customer service.
+            </p>
+        </div>
+
+        <div>
+            <p className='text-xl font-medium mb-5'>COMPANY</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
+            </ul>
+        </div>
+
+        <div>
+            <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>+xx-xxxxxxxxxx</li>
+                <li>support@bubunstore.com</li>
+            </ul>
+        </div>
+
+      </div>
+
+        <div>
+            <hr />
+            <p className='py-5 text-sm text-center'>Copyright 2026@Bubun Store.com - All Right Reserved.</p>
+        </div>
+
+    </div>
+  )
+}
+
+export default Footer
